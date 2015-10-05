639fafff18768914a01c15c808ad8ddd1f683a89
akdel <mehmet_akdel@hotmail.com>
2015-10-05 17:04:24 +0200

    added new script which will be used to see which pages are similar in wikipedia

--- wiki_similarities.py
+++ wiki_similarities.py
@@ -2,14 +2,27 @@ __author__ = 'SteamPowered'
 import requests as rq
 
 class Wiki:
-    def __init__(self):
-
+    def __init__(self, title_list):
         self.page_dict = {}
-
+        self.simi_matrix = []
+        for title in title_list:
+            self.page_dict[title] = ''
         resp = str(rq.get('http://www.wikipedia.org/'))
         if resp == '<Response [200]>':
             print "\nWikipedia is working!"
+        else:
+            print "Wikipedia or your internet connection is not responding!"
+
+    def fill_page_dict(self):
+        return None
+
+    def find_mems(self):
+        return None
+
+    def fill_sim_matrix(self):
+        return None
 
 
 if __name__ == "__main__":
-    wk = Wiki()
+    wk = Wiki(['a'])
+    print wk.page_dict
